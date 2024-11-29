8118a3290a11529cb9ee7993b9fa09b345a573d0
evgenijkrasnoperov <runyugin@mail.ru>
2024-11-29 14:55:39 +0300

    hw5

---
+++ src/main/java/ru/geekbrains/lesson5/hm5/Main.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson5.hm5;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+        System.out.println("привет");
+    }
+}
